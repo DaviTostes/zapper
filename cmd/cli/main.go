e00d8bfe447b77b8ab663408c47e997f974a8f14
Davi Tostes <davisiqueira591@gmail.com>
2025-09-09 22:19:50 -0300

    feat: client initial structure

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,67 +1,14 @@
-package cli
+package main
 
 import (
-	"context"
-	"fmt"
+	"gozapper/internal/whatsmeow"
 	"os"
-	"os/signal"
-	"syscall"
-
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/mdp/qrterminal"
-	"go.mau.fi/whatsmeow"
-	"go.mau.fi/whatsmeow/store/sqlstore"
-	"go.mau.fi/whatsmeow/types/events"
-	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
-func eventHandler(evt any) {
-	switch v := evt.(type) {
-	case *events.Message:
-		fmt.Println("Received Message: ", v.Info.Sender)
-	}
-}
-
 func main() {
-	dbLog := waLog.Stdout("Database", "", true)
-	ctx := context.Background()
-	container, err := sqlstore.New(ctx, "sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
-	if err != nil {
-		panic(err)
+	args := os.Args
+	switch args[1] {
+	case "connect":
+		whatsmeow.ConnectClient()
 	}
-
-	deviceStore, err := container.GetFirstDevice(ctx)
-	if err != nil {
-		panic(err)
-	}
-	clientLog := waLog.Stdout("Client", "", true)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
-	client.AddEventHandler(eventHandler)
-
-	if client.Store.ID == nil {
-		qrChan, _ := client.GetQRChannel(context.Background())
-		err = client.Connect()
-		if err != nil {
-			panic(err)
-		}
-
-		for evt := range qrChan {
-			if evt.Event == "code" {
-				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
-			} else {
-				fmt.Println("Login event:", evt.Event)
-			}
-		}
-	} else {
-		err = client.Connect()
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-
-	client.Disconnect()
 }
